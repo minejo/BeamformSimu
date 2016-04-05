ee8eb5844f6687b93e41ab6bbc363d60be7e1d25
Chao Li <jonathan.swjtu@gmail.com>
2016-04-05 09:02:18 +0800

    创建updatemap函数

---
+++ updatemap.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%
+%%% Author: Chao Li %%%
+%%%%%%%%%%%%%%%%%%%%%%%
+
+function [map, RL_pre, RW_pre] = updatemap(map,R0l,RL_pre,R0w,RW_pre,v0_w)
+%改变物体的map中映射的位置
+[x,y]=size(map);
+x_t=1:x;
+y_t=1:y;
+map(RL_pre,RW_pre)=-1;
+RL=find(x_t<=R0l, 1, 'last' );
+RW=find(y_t<=R0w, 1, 'last' );
+map(RL,RW)=v0_w;
+RL_pre=RL;
+RW_pre=RW;
+end
