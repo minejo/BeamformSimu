043a980588329c1a1788ebe8a69604c3047ce02f
Chao Li <jonathan.swjtu@gmail.com>
2016-04-06 03:10:13 +0800

    大波束方案测试完成

--- bigBeamFindObject.m
+++ bigBeamFindObject.m
@@ -15,8 +15,11 @@ for i = 1:big_num_l
         index_w = (beamPos_w-1)*big_beam/map_w + j;
         if(map(index_l,index_w) > -1)
             hasObject = 1;
-            L = index_l*map_l;
-            W = index_w*map_w;
+            %L = index_l*map_l;
+            %W = index_w*map_w;
+            %获取的位置取波束中心
+            L = (beamPos_l-1)*big_beam + 0.5*big_beam;
+            W = (beamPos_w-1)*big_beam + 0.5*big_beam;
             v = map(index_l,index_w);
         end
     end    
--- updatemap.m
+++ updatemap.m
@@ -7,10 +7,10 @@ function [map, RL_pre, RW_pre] = updatemap(map,R0l,RL_pre,R0w,RW_pre,v0_w,map_l,
 [x,y]=size(map);
 x_t=1:x;
 y_t=1:y;
-map(RL_pre/map_l,RW_pre/map_w)=-1;
+map(RL_pre,RW_pre)=-1;
 RL=find(x_t<=R0l/map_l, 1, 'last' );
 RW=find(y_t<=R0w/map_w, 1, 'last' );
 map(RL,RW)=abs(v0_w);
-RL_pre=RL*map_l;
-RW_pre=RW*map_w;
+RL_pre=RL;
+RW_pre=RW;
 end
